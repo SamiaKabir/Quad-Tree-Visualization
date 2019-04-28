8e52f99684d3244413d7f00235d6368f33dcbe7e
Samia <samialiya212@gmail.com>
2019-04-28 01:09:43 -0500

    space view done( without lowest limit checking and same point checking)

--- Scripts/createData.js
+++ Scripts/createData.js
@@ -1,149 +1,226 @@
-class Node {
-    constructor(x, y, corners, root, internal, leaf, t_l, t_r, b_l, b_r) {
-        this.point = {
-            x: x,
-            y: y
-        };
+class Quad {
+    constructor(point, corners, root, internal, t_l, t_r, b_l, b_r) {
+        this.point = point;
         this.corners = corners;
         this.isroot = root;
         this.isinternal = internal;
-        this.isleaf = leaf;
-        top_left=t_l;
-        top_right=t_r;
-        bottom_left=b_l;
-        bottom_right=b_r;
+        this.top_left=t_l;
+        this.top_right=t_r;
+        this.bottom_left=b_l;
+        this.bottom_right=b_r;
     
     }
-}
-
-
-
-
-function addNode(tree,node)
-{
-    
-    console.log(tree.point.x);
-    console.log(node.point.x);
-    if(node.point.x>tree.point.x)
-    {
-        if(node.point.y<tree.point.y)
-        {
-           //for drawing purpose
-            var c_p={};
-            c_p.x1= (tree.corners.x2-tree.corners.x1)/2;
-            c_p.x2= tree.corners.x2;
-            c_p.y1= tree.corners.y1;
-            c_p.y2= (tree.corners.y2-tree.corners.y1)/2;
-            node.corners= c_p;
     
 
-            //add the new node
-            tree.top_right=node;
-        }
+    get_point(){
 
-        else 
+       return this.point;
+    }
+
+    set_point(){ 
+        this.point=null;
+        return;
+
+    }
+
+    get_area(){
+      
+        return ((corners.x2-corners.x1)*(corners.y2-corners.y1));
+
+    }
+
+    set_isinternal(){
+
+        this.isinternal=true;
+        return;
+    }
+    
+    get_corner(){
+     return this.corners;
+    }
+
+    isinRange(pointab){
+       if((pointab.x>this.corners.x1) &&(pointab.x<this.corners.x2))
+       {
+           if ((pointab.y>this.corners.y1) &&(pointab.y<this.corners.y2))
+               return true;
+           else 
+               return false;
+       }
+
+       else return false;
+        
+    }
+    
+
+    splitinfour(pNode){
+        var c_x1= pNode.corners.x1;
+        var c_y1= pNode.corners.y1;
+        var c_x2= pNode.corners.x2;
+        var c_y2= pNode.corners.y2;
+        
+
+        //create the corner poimts for subquads
+        var c_t_le={};
+        var c_t_ri={};
+        var c_b_le={};
+        var c_b_ri={};
+
+
+        c_t_le.x1= c_x1;   c_t_le.y1= c_y1;   c_t_le.x2= c_x1+(c_x2-c_x1)/2;   c_t_le.y2= c_y1+(c_y2-c_y1)/2;  
+        c_t_ri.x1= c_x1+(c_x2-c_x1)/2;     c_t_ri.y1= c_y1;    c_t_ri.x2= c_x2; c_t_ri.y2= c_y1+(c_y2-c_y1)/2;  
+        c_b_le.x1= c_x1;   c_b_le.y1= c_y1+(c_y2-c_y1)/2;   c_b_le.x2= c_x1+(c_x2-c_x1)/2;   c_b_le.y2= c_y2;  
+        c_b_ri.x1= c_x1+(c_x2-c_x1)/2;     c_b_ri.y1= c_y1+(c_y2-c_y1)/2;    c_b_ri.x2= c_x2; c_b_ri.y2= c_y2;  
+
+        
+        //create the subquads
+
+        pNode.top_left= new Quad(null,c_t_le,false,false,null,null,null,null);
+        pNode.top_right= new Quad(null,c_t_ri,false,false,null,null,null,null);
+        pNode.bottom_left= new Quad(null,c_b_le,false,false,null,null,null,null);
+        pNode.bottom_right= new Quad(null,c_b_ri,false,false,null,null,null,null);
+
+        console.log(c_t_le);
+        console.log(c_t_ri);
+        console.log(c_b_le);     
+        console.log(c_b_ri);
+
+        //draw rectangle for these four
+        drawRect(c_t_le.x1, c_t_le.y1,c_t_le.x2-c_t_le.x1, c_t_le.y2-c_t_le.y1);
+        drawRect(c_b_le.x1, c_b_le.y1,c_b_le.x2-c_b_le.x1, c_b_le.y2-c_b_le.y1);
+        drawRect(c_t_ri.x1, c_t_ri.y1,c_t_ri.x2-c_t_ri.x1, c_t_ri.y2-c_t_ri.y1);
+        drawRect(c_b_ri.x1, c_b_ri.y1,c_b_ri.x2-c_b_ri.x1, c_b_ri.y2-c_b_ri.y1);
+
+
+    }
+
+    insert(newNode,pointIn){
+
+       if(newNode.isinternal==false && newNode.point!=null)  //split in subsquads when it's a leaf  node and there is already a point in it
+       {
+         newNode.isinternal=true;
+         newNode.splitinfour(newNode);
+
+
+      //  move current point to one of the subquads
+         if (newNode.top_left.isinRange(newNode.point))
+         {
+             newNode.top_left.point=newNode.point; 
+             newNode.point=null;
+         }
+        else if (newNode.top_right.isinRange(newNode.point))
+         {
+             newNode.top_right.point=newNode.point; 
+             newNode.point=null;
+         }
+        else if (newNode.bottom_left.isinRange(newNode.point))
+         {
+             newNode.bottom_left.point=newNode.point; 
+             newNode.point=null;
+         }
+        else if (newNode.bottom_right.isinRange(newNode.point))
         {
-            var c_p={};
-
-            c_p.x1= (tree.corners.x2-tree.corners.x1)/2;
-            c_p.x2= tree.corners.x2;
-            c_p.y2= tree.corners.y2;
-            c_p.y1= (tree.corners.y2-tree.corners.y1)/2;
-            node.corners= c_p;
-
-            tree.bottom_right=node;
+             newNode.bottom_right.point=newNode.point;
+             newNode.point=null;
         }
+        else;
+     // determine in which subquads the input point is gonna be
+        if (newNode.top_left.isinRange(pointIn))
+         {
+            if(newNode.top_left.point==null){
+                newNode.top_left.point=pointIn; 
+            }
+            else 
+            {
+                newNode.top_left.insert(newNode.top_left,pointIn);
+            }
+               
+            
+         }
+        else if (newNode.top_right.isinRange(pointIn))
+         {
+            if(newNode.top_right.point==null){
+                newNode.top_right.point=pointIn; 
+            }
+            else{
+                newNode.top_right.insert(newNode.top_right,pointIn);
+            }
            
+            
+         }
+        else if (newNode.bottom_left.isinRange(pointIn))
+         {
+            if(newNode.bottom_left.point==null){
+                newNode.bottom_left.point=pointIn; 
+            }
+            else
+            {
+                newNode.bottom_left.insert(newNode.bottom_left,pointIn);
+            }
+
+            
+         }
+        else if (newNode.bottom_right.isinRange(pointIn))
+        {
+            if(newNode.bottom_right.point!=null){
+                newNode.bottom_right.point=pointIn; 
+            }
+            else
+            {
+                newNode.bottom_right.insert(newNode.bottom_right,pointIn);
+            }
+            
+            
+        }
+        else;
+        
+       }
+
+       else if(newNode.isinternal==true)      //if it's an internal node , call insert recursively
+       {
+        
+  
+            if(newNode.top_left.isinRange(pointIn))
+            {
+                newNode.top_left.insert(newNode.top_left,pointIn);
+            }
+
+            else if(newNode.top_right.isinRange(pointIn))
+            {
+                newNode.top_right.insert(newNode.top_right,pointIn);
+            }
+
+            else if(newNode.bottom_right.isinRange(pointIn))
+            {
+                newNode.bottom_right.insert(newNode.bottom_right,pointIn);
+            }
+
+            else if(newNode.bottom_left.isinRange(pointIn))
+            {
+                newNode.bottom_left.insert(newNode.bottom_left,pointIn);
+            }
+
+            else;
+
+        
+       }
+   // simply add the point if it's a leaf node and there is no point in it
+       else  if(newNode.isinternal==false && newNode.point==null)   
+          newNode.point=pointIn;
+
+       else;
+
+   
+        return;
+
+
     }
 
-    else{
 
-        if(node.point.y<tree.point.y)
-        {
-            var c_p={};
+    search(){
 
-            c_p.x2= (tree.corners.x2-tree.corners.x1)/2;
-            c_p.x1= tree.corners.x1;
-            c_p.y1= tree.corners.y1;
-            c_p.y2= (tree.corners.y2-tree.corners.y1)/2;  
-            node.corners= c_p;
-           
-            tree.top_left=node;
-        }
 
-        else 
-        {
-            var c_p={};
-
-            c_p.x2= (tree.corners.x2-tree.corners.x1)/2;
-            c_p.x1= tree.corners.x1;
-            c_p.y2= tree.corners.y2;
-            c_p.y1= (tree.corners.y2-tree.corners.y1)/2;
-            node.corners= c_p;
-
-            tree.bottom_left=node;
-        }
-          
 
     }
-
 
 }
-
-
-function updateTree(tree,node)
-{
-
-
-    if(tree.isroot){
-        if(tree.bottom_left==null && tree.top_left==null && tree.top_right==null && tree.bottom_right==null){
-            tree.isinternal=true;
-             
-            var temp_node={}
-
-            var c_p={};
-
-            c_p.x2= (tree.corners.x2-tree.corners.x1)/2;
-            c_p.x1= tree.corners.x1;
-            c_p.y1= tree.corners.y1;
-            c_p.y2= (tree.corners.y2-tree.corners.y1)/2;  
-            temp_node.corners= c_p;
-            tree.top_left=temp_node;
-
-
-            c_p.x1= (tree.corners.x2-tree.corners.x1)/2;
-            c_p.x2= tree.corners.x2;
-            c_p.y1= tree.corners.y1;
-            c_p.y2= (tree.corners.y2-tree.corners.y1)/2;
-            temp_node.corners= c_p;
-            tree.top_right=temp_node;
-    
-            c_p.x2= (tree.corners.x2-tree.corners.x1)/2;
-            c_p.x1= tree.corners.x1;
-            c_p.y2= tree.corners.y2;
-            c_p.y1= (tree.corners.y2-tree.corners.y1)/2;
-            temp_node.corners= c_p;
-            tree.bottom_left=temp_node;
-
-
-            c_p.x1= (tree.corners.x2-tree.corners.x1)/2;
-            c_p.x2= tree.corners.x2;
-            c_p.y2= tree.corners.y2;
-            c_p.y1= (tree.corners.y2-tree.corners.y1)/2;
-            temp_node.corners= c_p;
-            tree.bottom_right=temp_node;
-
-            addNode(tree,node);
-        }
-
-        else
-        {
-            addNode(tree,node);
-        }
-    }
-
-    else ;
-
-
-}
-
--- Scripts/spaceview.js
+++ Scripts/spaceview.js
@@ -5,6 +5,33 @@ var Nodes={};
 var all_Nodes=[];
 var QTree={};
 
+
+//function to draw points
+function drawCircle(x, y, size) {
+    console.log('Drawing circle at', x, y, size);
+    svg.append("circle")
+        .attr('class', 'click-circle')
+        .attr("cx", x)
+        .attr("cy", y)
+        .attr("r", size);
+}
+
+
+//functions to draw rectangle 
+
+function drawRect(x,y,width,height){
+    console.log('Drawing new node at', x, y, width , height);
+    svg.append("rect")
+        .attr('class', 'node-rect')
+        .attr("x", x)
+        .attr("y", y)
+        .attr("width", width)
+        .attr("height",height)
+        .attr("fill", 'none')
+        .attr("stroke","black")
+        .attr("stroke-width",0.5);
+}
+
 var count=1;
 
 var svg = d3.select('svg'); 
@@ -15,30 +42,6 @@ function init() {
         return Math.floor(Math.random() * (max - min) + min);
     }
 
-    function drawCircle(x, y, size) {
-        console.log('Drawing circle at', x, y, size);
-        svg.append("circle")
-            .attr('class', 'click-circle')
-            .attr("cx", x)
-            .attr("cy", y)
-            .attr("r", size);
-    }
-
-
-
-
-    function drawRect(x,y,width,height){
-        console.log('Drawing new node at', x, y, width , height);
-        svg.append("rect")
-            .attr('class', 'node-rect')
-            .attr("x", x)
-            .attr("y", y)
-            .attr("width", width)
-            .attr("height",height)
-            .attr("fill", 'none')
-            .attr("stroke","black")
-            .attr("stroke-width",0.5);
-    }
 
 
     svg.on('click', function() {
@@ -47,7 +50,7 @@ function init() {
         var points={};
         var coords = d3.mouse(this);
     //    console.log(coords);
-        drawCircle(coords[0], coords[1], 5);
+        drawCircle(coords[0], coords[1], 2);
        // addNode();
         points.x=coords[0];
         points.y=coords[1];
@@ -60,27 +63,24 @@ function init() {
         var sizes   = document.getElementById("space_container"); // or other selector like querySelector()
         var container = sizes.getBoundingClientRect(); // get the bounding rectangle
         var corner_points={};
-       // console.log( container.width );
-       // console.log( container.height);
     
 
-        //create the 1st node
-        Nodes.id=1;
+        //create the root node node
         Nodes.point=points;
-        Nodes.top_left= null;
-        Nodes.bottom_left= null;
-        Nodes.top_right= null;
-        Nodes.bottom_right= null;
-        Nodes.isroot= true; 
-        Nodes.isinternal=false;
         corner_points.x1= 0;
         corner_points.y1= 0;
         corner_points.x2=600;
         corner_points.y2= 600;
         Nodes.corners=corner_points;
+        
+
+        let root= new Quad(points, corner_points,true, false, null,null,null,null);
+        console.log(root);
 
 
-        QTree.root=Nodes;
+        drawRect(corner_points.x1, corner_points.y1, 600,600);
+
+        QTree.root=root;
 
         all_Nodes.push({Nodes});
        // console.log(Nodes);
@@ -89,40 +89,14 @@ function init() {
     }
         
     //check if need to divide the space
-    else if(count>1){
-         var sizes   = document.getElementById("space_container"); // or other selector like querySelector()
-         var container = sizes.getBoundingClientRect(); // get the bounding rectangle
+    else if(count>1){         
+        
 
-        //  drawRect(0, 0, (container.width)/2,(container.height)/2);
-        //  drawRect(0, 300, (container.width)/2,(container.height)/2);
-        //  drawRect(300, 0, (container.width)/2,(container.height)/2);
-        //  drawRect(300, 300, (container.width)/2,(container.height)/2);
-
-        var Node={}; 
-
-        Node.id=count;
-        Node.point=points;
-        Node.top_left= null;
-        Node.bottom_left= null;
-        Node.top_right= null;
-        Node.bottom_right= null;
-        Node.isroot= false; 
-     
-        console.log(Node)
-
-
-        updateTree(QTree.root,Node);
- 
-
-        all_Nodes.push({Nodes});
-       // console.log(Nodes);
-        console.log(QTree);
+        //let newNode= new Quad(points, null,false, false, null,null,null,null);
+        QTree.root.insert(QTree.root,points);
+        console.log(QTree.root);
         count++;
 
-
-
-
-         count++;
 
     }
     else ;
